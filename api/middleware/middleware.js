0efe0e65fc5f69ecce011fec55a50f52abeff3fa
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-12 18:26:50 -0800

    validateUserId middleware function done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,42 @@
+const User = require('../users/users-model.js');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString();
+  console.log(`${timestamp}, Method: ${req.method}, URL: ${req.originalUrl}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({message: "user not found" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error validating user ID" });
+  }
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const Port = 9000;
+
+server.listen(Port, () => {
+    console.log('Listening on port 9000');
+})
